03f2eb449cc2265c16f18c1d98ddb6a30c0a9545
Sharath Sahadevan <sharath_sahadevan@hotmail.com>
2020-10-12 10:27:01 -0500

    Transaction updates
    Add multiple Transactions and view them in the UI. Added TransactionRestController to get the counts.

---
+++ bin/addTransactions.sh
@@ -0,0 +1,37 @@
+echo "post transactions/1"
+curl -X POST http://localhost:8081/transactions -H 'Content-type:application/json' -d '{"id": 1 , "date": "09/30/2020", "merchantName": "Walmart" , "amount": 15.23}'
+
+#!/bin/bash
+START=2
+END=100
+## save $START, just in case if we need it later ##
+i=$START
+var1='{"id":'
+var2=',"date":'
+merchantName=merchant_
+varAmountHdr=',"amount":'
+lastName=Doe_
+varMerchantNameHdr=',"merchantName":'
+middleName=Rich_
+currentDate='"09/09/2020"'
+var3='}'
+while [[ $i -le $END ]]
+do
+    echo "$i"
+    currentFirstName='"'$firstName$i'"'
+    currentMerchantName=$varMerchantNameHdr'"'$merchantName$i'"'
+    currentAmount=$varAmountHdr'"'$i.$i0'"'
+
+    #echo $var1$i$var2$i$var3
+    var4=$var1'"'$i'"'$var2$currentDate$currentMerchantName$currentAmount$var3
+    echo var4 is $var4
+    curl -X POST http://localhost:8081/transactions -H 'Content-type:application/json' -d $var4
+
+    ((i = i + 1))
+done
+
+echo " "
+echo " Number Of transactions is : "
+curl -X GET  http://localhost:8081/transactions/count
+echo " "
+echo "***  Script completed ***"
---
+++ src/main/java/com/ss/hazelcast/SampleApplication/payments/controller/TransactionRestController.java
@@ -0,0 +1,51 @@
+package com.ss.hazelcast.SampleApplication.payments.controller;
+
+import com.ss.hazelcast.SampleApplication.Util.HazelcastClientUtility;
+import com.ss.hazelcast.SampleApplication.payments.dao.Transaction;
+import com.ss.hazelcast.SampleApplication.payments.service.TransactionService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+
+
+@RestController
+public class TransactionRestController {
+
+    private static final String template = "Product is, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    // inject via application.properties
+    @Value("${welcome.message}")
+    private String message;
+
+    private List<String> tasks = Arrays.asList("a", "b", "c", "d", "e", "f", "g");
+
+    /* Get a Specific Key */
+    @GetMapping("/transactions/{id}")
+    public Transaction getTransaction(@PathVariable long id ) {
+
+        HazelcastClientUtility hazelcastClientUtility = new HazelcastClientUtility();
+        return (Transaction) hazelcastClientUtility.get("Transaction", String.valueOf(id) );
+    }
+
+
+    /* Gets a transactions count */
+    @GetMapping("transactions/count")
+    public String getTransactionsCount(Model model) {
+        TransactionService transactionService = new TransactionService() ;
+        int transactionCount =  transactionService.getTransactionsCount();
+        // model.addAttribute("transactionsCount", transactionCount);
+        return "Number of Transactions is :" + transactionCount;
+    }
+
+
+}
+
+
